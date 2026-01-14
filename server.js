f4038165bc3b9e98f521d01e2f2aa5f6628459f6
unknown <ade12peace@gmail.com>
2026-01-14 22:55:51 +0100

    dev

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ import connectToCloudinary from "./config/cloudinary.js";
 
 const PORT = process.env.PORT || 3800;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.get("/", (req, res) => {
   res.json({
     msg: "Welcome to Homepage",
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 connectToCloudinary();
